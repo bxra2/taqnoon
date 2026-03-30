a13911743b993afd359f4494548879899d59a209
Bxra2 <baraaashraf@protonmail.com>
2026-03-30 16:14:24 +0800

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,20 @@
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-static';
 
 export default {
     kit: {
         adapter: adapter({
             pages: 'build',
             assets: 'build',
-            fallback: 'index.html', // <- SPA fallback
+            fallback: 'index.html',
         }),
+
         alias: {
             $src: './src',
             $routes: './src/routes',
         },
-    },
-}
+
+        prerender: {
+            handleHttpError: 'ignore' // prevents build crashes on dynamic routes
+        }
+    }
+};
